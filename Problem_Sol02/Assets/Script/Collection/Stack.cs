b9953164a614ad7a185343524f13710dcc3f15ac
yunam20 <zxzxzx00000@naver.com>
2024-04-02 13:13:11 +0900

    small fix

--- Problem_Sol02/Assets/Script/Collection/Stack.cs
+++ Problem_Sol02/Assets/Script/Collection/Stack.cs
@@ -6,7 +6,6 @@ namespace Mc
     public class Stack<T> : MonoBehaviour
     {
         Queue<T> queue = new Queue<T>();
-        Queue<T> queue2 = new Queue<T>();
 
         public void Push(T data)
         {
@@ -16,6 +15,8 @@ namespace Mc
         // LinkedList의 헤드 부분을 삭제, insert는 front에서 이뤄지므로 head 부분을 삭제하면 선입후출 방식을 완성할 수 있음
         public T Pop()
         {
+            Queue<T> queue2 = new Queue<T>();
+
             while (queue.Count > 1)
             {
                 queue2.Enqueue(queue.Dequeue());
