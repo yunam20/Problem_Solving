2c2b7ddda2f79b05487d6920915fc2c16c3722ad
yunam20 <zxzxzx00000@naver.com>
2024-04-02 15:55:27 +0900

    star

---
+++ Problem_Sol02/Assets/Script/Problem05/StaticMeshGen.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(StaticMeshGen))]
+public class StaticMeshGenEditor : Editor
+{
+    //버튼만들기 예제
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        StaticMeshGen script = (StaticMeshGen)target;
+
+        if (GUILayout.Button("Generate Mesh"))
+        {
+            script.GenerateMesh();
+        }
+
+    }
+}
+
+//메쉬만들기 예제
+public class StaticMeshGen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void GenerateMesh()
+    {
+        Mesh mesh = new Mesh();
+
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3 (0.0f, 0.0f, 0.0f), //0
+            new Vector3 (-1.0f, -2.0f, 0.0f), //1
+            new Vector3 (1.0f, -2.0f, 0.0f), //2
+            new Vector3 (3.0f, -2.0f, 0.0f), //3
+            new Vector3 (2.0f, -4.0f, 0.0f), //4
+            new Vector3 (3.0f, -6.0f, 0.0f), //5
+            new Vector3 (0f, -5.0f, 0.0f), //6
+            new Vector3 (-3.0f, -6.0f, 0.0f), //7
+            new Vector3 (-2.0f, -4.0f, 0.0f), //8
+            new Vector3 (-3.0f, -2.0f, 0.0f), //9
+            new Vector3 (-1.0f, -2.0f, 0.0f), //10
+        };
+
+        mesh.vertices = vertices;
+
+        int[] triangleIndices = new int[]
+        {
+            0,2,1,
+            2,3,4,
+            4,5,6,
+            6,7,8,
+            8,9,10,
+            6,8,10,
+            10,2,6,
+            4,6,2
+        };
+
+        mesh.triangles = triangleIndices;
+
+        MeshFilter mf = this.AddComponent<MeshFilter>();
+        MeshRenderer mr = this.AddComponent<MeshRenderer>();
+
+        mf.mesh = mesh;
+    }
+}
--- Problem_Sol02/Assets/Script/TestScript.cs
+++ Problem_Sol02/Assets/Script/TestScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,9 +8,53 @@ public class TestScript : MonoBehaviour
     public Mc.Stack<int> testStack;
     public Mc.Queue<int> testQueue;
 
+    int CalculateGenerate(int num)
+    {
+        // 입력된 숫자를 문자열로 변환하여 각 자릿수를 분리
+        string numStr = num.ToString();
+        int digitSum = 0;
+
+        // 각 자릿수의 합 계산
+        foreach (char digitChar in numStr)
+        {
+            digitSum += (int)Char.GetNumericValue(digitChar);
+        }
+
+        // 원래 숫자와 각 자릿수의 합을 더하여 반환
+        return num + digitSum;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        List<int> value = new List<int>();
+
+        // CalculateGenerate 함수를 사용하여 value 리스트 초기화
+        for (int i = 0; i <= 5000; i++) // 0부터 100까지의 숫자로 수정
+        {
+            value.Add(CalculateGenerate(i));
+        }
+
+        // value 리스트의 각 요소 출력
+        foreach (int item in value)
+        {
+            Debug.Log(item);
+        }
+
+        int all = 0;
+
+        // value 리스트의 각 요소 중에서 CalculateGenerate 함수로 구할 수 없는 값을 누적하여 계산
+        for (int i = 0; i <= value.Count; i++) // value.Count로 수정
+        {
+            if (!value.Contains(i))
+            {
+                all += i;
+            }
+        }
+
+        Debug.Log("합: " + all);
+
+        /*
         testStack = new Mc.Stack<int>();
         testQueue = new Mc.Queue<int>();
 
@@ -26,5 +71,6 @@ public class TestScript : MonoBehaviour
         Debug.Log("Queue: " + testQueue.Dequeue());
         Debug.Log("Queue: " + testQueue.Dequeue());
         Debug.Log("Queue: " + testQueue.Dequeue());
+        */
     }
 }
